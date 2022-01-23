3f48395b5b7e75430c92f66a0e31960b6992e194
Gabriel <gamcode98@gmail.com>
2022-01-23 17:34:09 -0300

    ready ☔

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    colors: {
+      //Colors
+      whiteCustom: "hsl(0, 0%, 100%)",
+      lightgray: "hsl(212, 45%, 89%)",
+      grayishblue: "hsl(220, 15%, 55%)",
+      darkblue: "hsl(218, 44%, 22%)",
+    },
+    fontFamily: {
+      //Wrights: 400, 700
+      Outfit: "Outfit",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
